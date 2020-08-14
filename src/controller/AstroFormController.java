690f6e2fe540dec9c807bb11e0efcbf3bc3bb06d
Arshia9 <arshiarshu5@gmail.com>
2020-08-14 14:46:54 +0530

    done

--- src/controller/AstroFormController.java
+++ src/controller/AstroFormController.java
@@ -2,12 +2,17 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
+import model.User;
+import service.AstroCalculator;
 
 
 @WebServlet(urlPatterns = {"/entervalue"})
@@ -20,13 +25,19 @@ public class AstroFormController extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/* 
+
 		String name=request.getParameter("name");
+	
 		String gender=request.getParameter("gender");
 		String date=request.getParameter("dob1");
 		String month=request.getParameter("dob2");
 		String year=request.getParameter("dob3");
-		String dob = date+"-"+month+"-"+year;	
+		String dob = date+"-"+month+"-"+year;
+			
+		User  u=new User(name,gender,dob);
+		AstroCalculator ac = new AstroCalculator();
+		String astrosign = ac.findSign(u);
+		System.out.println(astrosign);
 		
 		if(astrosign.equals("Aquarius")){
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
@@ -88,6 +99,6 @@ public class AstroFormController extends HttpServlet {
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/virgo.svg'>");
 			rd.forward(request, response);
-		}*/
+		}
 	}
 }
--- src/model/User.java
+++ src/model/User.java
@@ -1,3 +1,60 @@
 package model;
 
 //Type your code here
+public class User{
+	public String name;
+
+	public String dob;
+	public String gender;
+	
+
+	
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getDob() {
+		return dob;
+	}
+
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+	public User(String name, String gender, String dob) {
+		super();
+		this.name = name;
+		this.gender = gender;
+		this.dob = dob;
+		
+	}
+	
+	
+	
+	
+
+
+//	public User(String string, String string2, String string3) {
+//		// TODO Auto-generated constructor stub
+//	}
+	
+}
